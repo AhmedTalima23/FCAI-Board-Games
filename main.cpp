1d219e64cce5894a98c3e0d969c647acca1dc575
Ahmed Talima <162712731+AhmedTalima23@users.noreply.github.com>
2024-12-12 18:34:34 +0200

    Update main.cpp
    numbers game added

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
+#include <set>
 #include "BoardGame_Classes.h"
 #include "connect4.h"
 #include "4x4.h"
 #include "Misere_X_O.h"
+#include "TICTACTOE_H.h"
 
 using namespace std;
 
@@ -155,6 +157,86 @@ void MisereXO()
     gameManager.run();
 }
 
+void numbersXO()
+{
+    TicTacToeBoardd<int> board;
+    TicTacToePlayerr<int> player1("Player 1", 1);
+    TicTacToePlayerr<int> player2("Player 2", 2);
+
+    player1.setBoard(&board);
+    player2.setBoard(&board);
+
+    int x, y, col;
+    Player<int> *current_player = &player1;
+    bool gameOver = false;
+
+    set<int> usedNumbers;
+
+    board.display_board();
+
+    while (!gameOver)
+    {
+        cout << current_player->getname() << "'s turn (" << current_player->getsymbol() << "):\n";
+        cout << "Enter the number you want to play (between 1 and 9): ";
+        cin >> x;
+        cout << "Enter the row and column (1-3): ";
+        cin >> y >> col;
+
+        if (x < 1 || x > 9 || y < 1 || y > 3 || col < 1 || col > 3)
+        {
+            cout << "Invalid input, please try again.\n";
+            continue;
+        }
+
+        if (usedNumbers.find(x) != usedNumbers.end())
+        {
+            cout << "This number has already been played, try again.\n";
+            continue;
+        }
+
+        if (board.get_board_value(y - 1, col - 1) != 0)
+        {
+            cout << "This cell is already occupied, please choose another one.\n";
+            continue;
+        }
+
+        if (current_player == &player1 && x % 2 == 0)
+        {
+            cout << "Player 1 must enter an odd number.\n";
+            continue;
+        }
+        else if (current_player == &player2 && x % 2 != 0)
+        {
+            cout << "Player 2 must enter an even number.\n";
+            continue;
+        }
+
+        if (!board.update_board(y - 1, col - 1, x))
+        {
+            cout << "Invalid move, try again.\n";
+            continue;
+        }
+
+        usedNumbers.insert(x);
+
+        board.display_board();
+
+        if (board.is_win())
+        {
+            cout << current_player->getname() << " wins!\n";
+            gameOver = true;
+        }
+        else if (board.is_draw())
+        {
+            cout << "It's a draw!\n";
+            gameOver = true;
+        }
+        else
+        {
+            current_player = (current_player == &player1) ? &player2 : &player1;
+        }
+    }
+}
 int main()
 {
     int gameChoice;
@@ -166,7 +248,8 @@ int main()
         cout << "1. Connect4\n";
         cout << "2. Tic-Tac-Toe (4x4)\n";
         cout << "3. Misere X-O (Tic-Tac-Toe)\n";
-        cout << "4. Exit\n";
+        cout << "4. Numbers\n";
+        cout << "5. Exit\n";
 
         cin >> gameChoice;
 
@@ -183,6 +266,10 @@ int main()
             break;
 
         case 4:
+            numbersXO();
+            break;
+
+        case 5:
             cout << "Thank you for playing! Goodbye.\n";
             return 0;
         default:
@@ -190,3 +277,4 @@ int main()
         }
     }
 }
+
